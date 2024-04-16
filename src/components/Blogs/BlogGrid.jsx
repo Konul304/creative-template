287ebd6f090904333f09c15e2ab4a2a3893da904
Konul304 <memmedovakonul270@gmail.com>
2024-04-16 09:33:34 +0400

    add tabs to news page

--- src/components/Blogs/BlogGrid.jsx
+++ src/components/Blogs/BlogGrid.jsx
@@ -1,10 +1,11 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { getCases } from '@/app/(api)/api';
 import { useQuery } from 'react-query';
+import initIsotope from '@/common/initIsotopePortfolio';
 
-const BlogGrid = () => {
+const BlogGrid = ({ grid = 3, filterPosition, hideFilter }) => {
   const { data, isLoading, isError } = useQuery(
     ['casesData'],
     async () => await getCases(),
@@ -13,130 +14,123 @@ const BlogGrid = () => {
       refetchOnMount: false,
     }
   );
+  useEffect(() => {
+    // setTimeout(() => {
+    initIsotope();
+    // }, 500);
+  }, [data]);
+
   return (
-    <section className="blog-pg blog section-padding pt-0">
+    <section
+      className={`${
+        grid ? (grid === 3 ? 'three-column' : null) : null
+      } portfolio  pb-70`}
+    >
       <div className="container">
-        <div className="posts">
-          <div className="row">
-            {data?.map((item) => {
-              const img_url =
-                'https://project141.s3.eu-north-1.amazonaws.com/' +
-                item?.logoLink;
-              return (
-                <div className="col-lg-4" key={item.id}>
-                  <div className="item mb-80 wow fadeInUp" data-wow-delay=".3s">
-                    <a href={`/cases/cases-dark/${item?.id}`} className="img">
-                      <img src={img_url} alt="" />
-                    </a>
-                    <div className="cont">
-                      <div>
-                        <div className="info">
-                          {/* <Link href="/blog/blog-dark" className="date">
+        <div className="row">
+          {!hideFilter && (
+            <div className={`filtering text-center col-12`}>
+              <div className="filter">
+                <span
+                  data-filter=".presentation"
+                  className={data?.length > 0 ? `active` : ''}
+                >
+                  {' '}
+                  Öz xəbərlərimiz{' '}
+                </span>
+                <span data-filter=".videos">Digərlər </span>
+                {/* <span data-filter=".web">Mobile App</span>
+                <span data-filter=".graphic">Creative</span> */}
+              </div>
+            </div>
+          )}
+          <div
+            className="gallery full-width"
+            style={{ minHeight: '400px', display: 'flex' }}
+          >
+            <div className="posts">
+              <div className="row">
+                {data?.map((item, index) => {
+                  const img_url =
+                    'https://project141.s3.eu-north-1.amazonaws.com/' +
+                    item?.logoLink;
+                  return (
+                    <div
+                      key={index}
+                      className={`col-md-4
+                       items presentation wow fadeInUp`}
+                      data-wow-delay=".4s"
+                    >
+                      <div className="" key={item.id}>
+                        <div
+                          className="item mb-80 wow fadeInUp"
+                          data-wow-delay=".3s"
+                        >
+                          <a
+                            href={`/cases/cases-dark/${item?.id}`}
+                            className="img"
+                          >
+                            <img
+                              style={{ height: '277px' }}
+                              src={img_url}
+                              alt=""
+                            />
+                          </a>
+                          <div className="cont">
+                            <div>
+                              <div className="info">
+                                {/* <Link href="/blog/blog-dark" className="date">
                           <span>
                             <i>{blogItem.date.day}</i>
                             {blogItem.date.month}
                           </span>
                         </Link> */}
-                          {/* <span>/</span> */}
-                          {item.tagNames.map((tagItem, index) => {
-                            return (
-                              <>
+                                {/* <span>/</span> */}
+                                {item.tagNames.map((tagItem, index) => {
+                                  return (
+                                    <>
+                                      <Link
+                                        key={index}
+                                        href={`/cases/cases-dark/${item?.id}`}
+                                        className="tag"
+                                      >
+                                        <span
+                                          className="wow color-font fw-700 "
+                                          key={index}
+                                        >
+                                          {tagItem}
+                                        </span>
+                                        &nbsp;&nbsp;&nbsp;
+                                      </Link>
+                                    </>
+                                  );
+                                })}
+                              </div>
+                              <h5>
                                 <Link
-                                  key={index}
                                   href={`/cases/cases-dark/${item?.id}`}
-                                  className="tag"
+                                  // style={{ color: '#5f5f5f' }}
                                 >
-                                  <span
-                                    className="wow color-font fw-700 "
-                                    key={index}
-                                  >
-                                    {tagItem}
-                                  </span>
-                                  &nbsp;&nbsp;&nbsp;
+                                  {item.title.substr(0, 55) + '...'}
                                 </Link>
-                              </>
-                            );
-                          })}
-                        </div>
-                        <h5>
-                          <Link
-                            href={`/cases/cases-dark/${item?.id}`}
-                            // style={{ color: '#5f5f5f' }}
-                          >
-                            {item.title.substr(0, 55) + '...'}
-                          </Link>
-                        </h5>
-                        <div className="btn-more">
-                          <Link
-                            href={`/cases/cases-dark/${item?.id}`}
-                            className="simple-btn"
-                          >
-                            Read More
-                          </Link>
+                              </h5>
+                              <div className="btn-more">
+                                <Link
+                                  href={`/cases/cases-dark/${item?.id}`}
+                                  className="simple-btn"
+                                >
+                                  Read More
+                                </Link>
+                              </div>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              );
-            })}
-            {/* {blogs.map((blogItem) => (
-              <div className="col-lg-4" key={blogItem.id}>
-                <div className="item mb-80 wow fadeInUp" data-wow-delay=".3s">
-                  <a href="/blog-details/blog-details-dark" className="img">
-                    <img src={blogItem.image} alt="" />
-                  </a>
-                  <div className="cont">
-                    <div>
-                      <div className="info">
-                        <Link href="/blog/blog-dark" className="date">
-                          <span>
-                            <i>{blogItem.date.day}</i>
-                            {blogItem.date.month}
-                          </span>
-                        </Link>
-                        <span>/</span>
-                        {blogItem.tags.map((tag, index) => (
-                          <Link
-                            key={index}
-                            href="/blog/blog-dark/"
-                            className="tag"
-                          >
-                            <span>{tag}</span>
-                          </Link>
-                        ))}
-                      </div>
-                      <h5>
-                        <Link href="/blog-details/blog-details-dark">
-                          {blogItem.title.substr(0, 55) + '...'}
-                        </Link>
-                      </h5>
-                      <div className="btn-more">
-                        <Link
-                          href="/blog-details/blog-details-dark"
-                          className="simple-btn"
-                        >
-                          Read More
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                  );
+                })}
               </div>
-            ))} */}
-            {/* <div className="pagination">
-              <span className="active">
-                <Link href={`/blog/blog-dark`}>1</Link>
-              </span>
-              <span>
-                <Link href={`/blog/blog-dark`}>2</Link>
-              </span>
-              <span>
-                <Link href={`/blog/blog-dark}`}>
-                  <i className="fas fa-angle-right"></i>
-                </Link>
-              </span>
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
